97abf9dfccb8122c468807ea52068778aedb7c1e
dDomovoj <dmitryduleba@gmail.com>
2022-06-08 20:06:06 +0200

    [add] base project

---
+++ SubsTemplate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SubsTemplate
+//
+//  Created by dDomovoj on 6/8/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
